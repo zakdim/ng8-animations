a02e1d71efd60b584e604c06f8bc4494e380f251
Dmitri Zakharov <dmitri.zakharov@gmail.com>
2020-03-21 21:10:11 -0400

    26-453 Setting up the starting project

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -2,9 +2,12 @@ import { Component } from '@angular/core';
 
 @Component({
   selector: 'app-root',
-  templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+  templateUrl: './app.component.html'
 })
 export class AppComponent {
-  title = 'ng8-animations';
+  list = ['Milk', 'Sugar', 'Bread'];
+
+    onAdd(item) {
+      this.list.push(item);
+    }
 }
